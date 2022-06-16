d98e6a17dc8147868e635d03d78b5a0b95c2fc84
ciarancooke9 <ciaran.cooke9@mail.dcu.ie>
2022-06-16 06:53:00 -0700

    drum sound added

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ for (var i = 0; i<document.querySelectorAll(".drum").length; i++){
 };
 
 function handleClick() {
-  alert("I got clicked");
+  var audio = new Audio('sounds/tom-1.mp3');
+  audio.play();
 }
